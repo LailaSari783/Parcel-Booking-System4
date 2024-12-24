0169911c89baee130204345ff4bae3da7e93c747
LailaSari783 <d097782@siswa.upsi.edu.my>
2024-12-24 16:13:08 +0800

    Laravel

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/staff/home', [HomeController::class, 'index'])->name('staff.home');
